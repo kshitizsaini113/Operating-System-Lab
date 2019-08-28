aab68ea9eb1a90c1c06b0525b6c61b45a85ad160
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-08-28 21:53:07 +0530

    Arguments studied

--- practice/file4.sh
+++ practice/file4.sh
@@ -7,3 +7,10 @@ echo $0 $1 $2 $3
 args=("$@")
 
 echo ${args[0]} ${args[1]} ${args[2]} ${args[3]}
+#prints specified items only
+
+echo $@
+#prints all arguments
+
+echo $#
+#prints number of arguments passed
