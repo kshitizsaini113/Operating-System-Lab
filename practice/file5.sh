20bd249e07c6bd7050de7184ba4fea355854755e
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-08-28 23:35:47 +0530

    if condition studied

---
+++ practice/file5.sh
@@ -0,0 +1,21 @@
+#bin/bash
+
+count=5
+
+if [ $count -eq 5 ]
+then
+  echo "Condition is true"
+fi
+
+
+if (( $count <= 5 ))
+then
+  echo "Condition is true"
+fi
+
+word='a'
+
+if [[ $word < "b" ]]
+then
+  echo "Condition is true"
+fi
