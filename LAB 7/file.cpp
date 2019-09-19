e51d7912cc13b6505d9fb555aebb7bdf3653fca3
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-09-19 14:58:57 +0530

    Asking for burst time

--- LAB 7/file.cpp
+++ LAB 7/file.cpp
@@ -5,5 +5,16 @@ using namespace std;
 
 int main()
 {
+  int process[4];
+  int burst_time[4];
+  int waiting_time[4];
+  int turn_around_time[4];
+
+  cout<<"Enter burst time for four processes : "<<endl;
+  for(int i=0;i<4;i++)
+  {
+    process[i]=i;
+    cin>>burst_time[i];
+  }
   return 0;
 }
