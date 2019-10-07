510a6cb466a03c10a6dcb49934457090374add47
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-10-07 11:49:50 +0530

    code reversed

--- practice/sjf_with_at.cpp
+++ practice/sjf_with_at.cpp
@@ -1,55 +1,92 @@
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-void main()
+#include<iostream>
+using namespace std;
+int mat[10][6];
+
+void swap(int *a, int *b)
 {
-    int et[20],at[10],n,i,j,temp,st[10],ft[10],wt[10],ta[10];
-    int totwt=0,totta=0;
-    float awt,ata;
-    char pn[10][10],t[10];
-    //clrscr();
-    printf("Enter the number of process:");
-    scanf("%d",&n);
-    for(i=0; i<n; i++)
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void arrangeArrival(int num, int mat[][6])
+{
+    for(int i=0; i<num; i++)
     {
-        printf("Enter process name, arrival time& execution time:");
-        //flushall();
-        scanf("%s%d%d",pn[i],&at[i],&et[i]);
-    }
-    for(i=0; i<n; i++)
-        for(j=0; j<n; j++)
+        for(int j=0; j<num-i-1; j++)
         {
-            if(et[i]<et[j])
+            if(mat[j][1] > mat[j+1][1])
             {
-                temp=at[i];
-                at[i]=at[j];
-                at[j]=temp;
-                temp=et[i];
-                et[i]=et[j];
-                et[j]=temp;
-                strcpy(t,pn[i]);
-                strcpy(pn[i],pn[j]);
-                strcpy(pn[j],t);
+                for(int k=0; k<5; k++)
+                {
+                    swap(mat[j][k], mat[j+1][k]);
+                }
             }
         }
-    for(i=0; i<n; i++)
-    {
-        if(i==0)
-            st[i]=at[i];
-        else
-            st[i]=ft[i-1];
-        wt[i]=st[i]-at[i];
-        ft[i]=st[i]+et[i];
-        ta[i]=ft[i]-at[i];
-        totwt+=wt[i];
-        totta+=ta[i];
     }
-    awt=(float)totwt/n;
-    ata=(float)totta/n;
-    printf("\nPname\tarrivaltime\texecutiontime\twaitingtime\ttatime");
-    for(i=0; i<n; i++)
-        printf("\n%s\t%5d\t\t%5d\t\t%5d\t\t%5d",pn[i],at[i],et[i],wt[i],ta[i]);
-    printf("\nAverage waiting time is:%f",awt);
-    printf("\nAverage turnaroundtime is:%f",ata);
-    getch();
+}
+
+void completionTime(int num, int mat[][6])
+{
+    int temp, val;
+    mat[0][3] = mat[0][1] + mat[0][2];
+    mat[0][5] = mat[0][3] - mat[0][1];
+    mat[0][4] = mat[0][5] - mat[0][2];
+
+    for(int i=1; i<num; i++)
+    {
+        temp = mat[i-1][3];
+        int low = mat[i][2];
+        for(int j=i; j<num; j++)
+        {
+            if(temp >= mat[j][1] && low >= mat[j][2])
+            {
+                low = mat[j][2];
+                val = j;
+            }
+        }
+        mat[val][3] = temp + mat[val][2];
+        mat[val][5] = mat[val][3] - mat[val][1];
+        mat[val][4] = mat[val][5] - mat[val][2];
+        for(int k=0; k<6; k++)
+        {
+            swap(mat[val][k], mat[i][k]);
+        }
+    }
+}
+
+int main()
+{
+    int num, temp;
+
+    cout<<"Enter number of Process: ";
+    cin>>num;
+
+    cout<<"...Enter the process ID...\n";
+    for(int i=0; i<num; i++)
+    {
+        cout<<"...Process "<<i+1<<"...\n";
+        cout<<"Enter Process Id: ";
+        cin>>mat[i][0];
+        cout<<"Enter Arrival Time: ";
+        cin>>mat[i][1];
+        cout<<"Enter Burst Time: ";
+        cin>>mat[i][2];
+    }
+
+    cout<<"Before Arrange...\n";
+    cout<<"Process ID\tArrival Time\tBurst Time\n";
+    for(int i=0; i<num; i++)
+    {
+        cout<<mat[i][0]<<"\t\t"<<mat[i][1]<<"\t\t"<<mat[i][2]<<"\n";
+    }
+
+    arrangeArrival(num, mat);
+    completionTime(num, mat);
+    cout<<"Final Result...\n";
+    cout<<"Process ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n";
+    for(int i=0; i<num; i++)
+    {
+        cout<<mat[i][0]<<"\t\t"<<mat[i][1]<<"\t\t"<<mat[i][2]<<"\t\t"<<mat[i][4]<<"\t\t"<<mat[i][5]<<"\n";
+    }
 }
