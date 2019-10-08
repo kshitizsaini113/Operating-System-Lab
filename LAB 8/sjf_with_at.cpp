e5a02d51531e81f12ce5716178dd19bb5c0bbeb8
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-10-08 09:11:21 +0530

    sort burst time and print gantt chart function removed

--- LAB 8/sjf_with_at.cpp
+++ LAB 8/sjf_with_at.cpp
@@ -30,25 +30,6 @@ void sort_arrival_time(int arrival_time[],int process[],int burst_time[])
   }
 }
 
-void sort_burst_time(int arrival_time[],int process[],int burst_time[])
-{
-  for(int i=0;i<4;i++)
-  {
-    for(int j=i+1;j<4;j++)
-    {
-      if(arrival_time[i]==arrival_time[j])
-      {
-        if(burst_time[i]>burst_time[j])
-        {
-          swap(process[i],process[j]);
-          swap(arrival_time[i],arrival_time[j]);
-          swap(burst_time[i],burst_time[j]);
-        }
-      }
-    }
-  }
-}
-
 void sjf_operations(int waiting_time[],int burst_time[],int turn_around_time[])
 {
   waiting_time[0]=0;
@@ -76,31 +57,6 @@ void print_table(int process[],int burst_time[],int waiting_time[],int turn_arou
   cout<<endl<<endl;
 }
 
-void print_gantt_chart(int burst_time[],int turn_around_time[],int process[])
-{
-  for(int i=0;i<4;i++)
-  {
-    cout<<"|"<<process[i];
-    for(int j=0;j<burst_time[i];j++)
-    {
-      cout<<" ";
-    }
-  }
-  cout<<"|"<<endl;
-  cout<<"0";
-  for(int i=0;i<4;i++)
-  {
-    cout<<" ";
-    for(int j=0;j<burst_time[i];j++)
-    {
-      cout<<" ";
-    }
-    cout<<turn_around_time[i];
-  }
-
-  cout<<endl<<endl;
-}
-
 int main()
 {
   int process[4];
@@ -114,13 +70,9 @@ int main()
 
   sort_arrival_time(arrival_time,process,burst_time);
 
-  sort_burst_time(arrival_time,process,burst_time);
-
   sjf_operations(waiting_time,burst_time,turn_around_time);
 
   print_table(process,burst_time,waiting_time,turn_around_time,arrival_time);
-
-  print_gantt_chart(burst_time,turn_around_time,process);
-
+  
   return 0;
 }
