9439da3dcd3a3873d589b0894a8e4ed3cf63c578
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-11-07 15:22:11 +0530

    Print function created

--- LAB 9/round_robin.cpp
+++ LAB 9/round_robin.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_table(int process[],int burst_time[],int waiting_time[],int turn_around_time[],int arrival_time[])
+{
+  cout<<endl<<endl;
+
+  cout<<"Process"<<" \t"<<"Arrival Time"<<" \t"<<"Burst Time"<<" \t"<<"Waiting Time"<<" \t"<<"Turn Around Time"<<endl;
+  for(int i=0;i<4;i++)
+  {
+    cout<<process[i]<<"\t\t"<<arrival_time[i]<<"\t\t"<<burst_time[i]<<"\t\t"<<waiting_time[i]<<"\t\t"<<turn_around_time[i]<<endl;
+  }
+
+  cout<<endl<<endl;
+}
+
 int main()
 {
   return 0;
