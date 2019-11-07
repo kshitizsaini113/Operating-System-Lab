6ee1d247b86a5920b72c3e507aa0365bd58ff4d3
Kshitiz <kshitizsaini.rtk@gmail.com>
2019-11-07 15:32:22 +0530

    code modified

--- LAB 9/round_robin.cpp
+++ LAB 9/round_robin.cpp
@@ -13,14 +13,14 @@ void input_burst_time(int burst_time[],int process[])
   }
 }
 
-void print_table(int process[],int burst_time[],int waiting_time[],int turn_around_time[],int arrival_time[])
+void print_table(int process[],int burst_time[],int waiting_time[],int turn_around_time[])
 {
   cout<<endl<<endl;
 
-  cout<<"Process"<<" \t"<<"Arrival Time"<<" \t"<<"Burst Time"<<" \t"<<"Waiting Time"<<" \t"<<"Turn Around Time"<<endl;
+  cout<<"Process"<<" \t"<<" \t"<<"Burst Time"<<" \t"<<"Waiting Time"<<" \t"<<"Turn Around Time"<<endl;
   for(int i=0;i<4;i++)
   {
-    cout<<process[i]<<"\t\t"<<arrival_time[i]<<"\t\t"<<burst_time[i]<<"\t\t"<<waiting_time[i]<<"\t\t"<<turn_around_time[i]<<endl;
+    cout<<process[i]<<"\t\t"<<burst_time[i]<<"\t\t"<<waiting_time[i]<<"\t\t"<<turn_around_time[i]<<endl;
   }
 
   cout<<endl<<endl;
